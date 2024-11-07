8996dbdb02ccfcf62ac5d0f4eb813dddd90de43a
Oneyker21 <oneygaleano21@gmail.com>
2024-11-07 15:32:15 -0600

    conexion a bd

---
+++ CONFIG/FirebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXQSMOLI_AVxZR_3jJMpBp-Bx35RaVLUE",
+  authDomain: "rentavehiculos-a15b0.firebaseapp.com",
+  projectId: "rentavehiculos-a15b0",
+  storageBucket: "rentavehiculos-a15b0.firebasestorage.app",
+  messagingSenderId: "230713319603",
+  appId: "1:230713319603:web:98840fdbf8550647dad0c6"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
